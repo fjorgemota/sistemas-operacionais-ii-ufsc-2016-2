b4a8b5cdccba0422dc7b4c380b768d39dfbdea2a
Fernando Jorge Mota <f.j.mota@hotmail.com>
2016-08-31 02:44:17 +0000

    first commit

---
+++ src/abstraction/semaphore.cc
@@ -0,0 +1,51 @@
+// EPOS Semaphore Abstraction Implementation
+
+#include <semaphore.h>
+
+__BEGIN_SYS
+
+Semaphore::Semaphore(int v): _value(v)
+{
+    db<Synchronizer>(TRC) << "Semaphore(value=" << _value << ") => " << this << endl;
+}
+
+
+Semaphore::~Semaphore()
+{
+    db<Synchronizer>(TRC) << "~Semaphore(this=" << this << ")" << endl;
+}
+
+
+void Semaphore::p()
+{
+    db<Synchronizer>(TRC) << "Semaphore::p(this=" << this << ",value=" << _value << ")" << endl;
+
+    fdec(_value);
+    //while(_value < 0) //remove while...
+    begin_atomic();
+    if(_value < 0) { // Insert a lock and unlock to grant that nothing will change.
+        //sleep();
+        block();
+    } else {
+        end_atomic();
+    }
+        
+}
+
+
+void Semaphore::v()
+{
+    db<Synchronizer>(TRC) << "Semaphore::v(this=" << this << ",value=" << _value << ")" << endl;
+
+    finc(_value);
+    begin_atomic();
+    if(_value < 1) {
+        //wakeup(); //to enable interruptions. Magically, the process will execute?
+        release();
+        //notify to remove process from the queue.
+    } else {
+        end_atomic();
+    }
+}
+
+__END_SYS
